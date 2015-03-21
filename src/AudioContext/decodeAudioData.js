86dceb850055ec5b99ca6487206267332b31ba5e
mohayonao <mohayonao@gmail.com>
2015-03-21 11:10:35 +0900

    add AudioContext.prototype.decodeAudioData

---
+++ src/AudioContext/decodeAudioData.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let OfflineAudioContext = global.OfflineAudioContext;
+let audioContext = new OfflineAudioContext(1, 1, 44100);
+let isPromiseBased = false;
+
+try {
+  let audioData = new Uint8Array(0).buffer;
+  let nop = () => {};
+  isPromiseBased = !!audioContext.decodeAudioData(audioData, nop);
+} catch (e) {}
+
+if (!isPromiseBased) {
+  let AudioContext = global.AudioContext;
+  let decodeAudioData = AudioContext.prototype.decodeAudioData;
+
+  //// ### AudioContext.prototype.decodeAudioData
+  //// Asynchronously decodes the audio file data contained in the ArrayBuffer.
+  ////
+  //// #### Parameters
+  //// - `audioData: ArrayBuffer` - An ArrayBuffer containing compressed audio data
+  //// - `successCallback: function` _(optional)_ - A callback function which will be invoked when the decoding is finished.
+  //// - `errorCallback: function` _(optional)_ - A callback function which will be invoked if there is an error decoding the audio file.
+  ////
+  //// #### Return
+  //// - `Promise<AudioBuffer>`
+  AudioContext.prototype.decodeAudioData = function(audioData, successCallback, errorCallback) {
+    return new Promise((resolve, reject) => {
+      return decodeAudioData.call(this, audioData, resolve, reject);
+    }).then(successCallback, errorCallback);
+  };
+  AudioContext.prototype.decodeAudioData.original = decodeAudioData;
+}
---
+++ test/AudioContext/decodeAudioData.js
@@ -0,0 +1,72 @@
+(function() {
+  "use strict";
+
+  require("../bootstrap");
+  require("../../src/AudioContext/decodeAudioData");
+
+  describe("AudioContext.prototype.decodeAudioData", function() {
+    var context;
+    if (!!global.AudioContext.prototype.decodeAudioData.original) {
+      context = "shim";
+    } else {
+      context = "native";
+    }
+    describe(context, function() {
+      function validAudioData() {
+        return new Uint32Array([
+          1179011410,44,1163280727,544501094,16,131073,44100,176400,1048580,1635017060,8,0,0
+        ]).buffer;
+      }
+      function invalidAudioData() {
+        return new Uint32Array(64).buffer;
+      }
+      describe("(validAudioData: ArrayBuffer): Promise<AudioBuffer>", function() {
+        it("should resolve with an AudioBuffer", function() {
+          var promise = audioContext.decodeAudioData(validAudioData());
+
+          assert(promise instanceof Promise, "should return a Promise");
+
+          return promise.then(function(audioBuffer) {
+            assert(audioBuffer instanceof global.AudioBuffer, "should reject with an AudioBuffer");
+          });
+        });
+      });
+      describe("(invalidAudioData: ArrayBuffer): Promise<AudioBuffer", function() {
+        it("should reject", function() {
+          var promise = audioContext.decodeAudioData(invalidAudioData());
+
+          assert(promise instanceof Promise, "should return a Promise");
+
+          return promise.then(function() {
+            assert(false, "NOT REACHED");
+          }, function() {
+            assert(true, "should reject");
+          });
+        });
+      });
+      describe("(validAudioData: ArrayBuffer, successCallback: function): Promise<AudioBuffer>", function() {
+        it("should call the successCallback with an AudioBuffer", function(done) {
+          var promise = audioContext.decodeAudioData(validAudioData(), function(audioBuffer) {
+            assert(audioBuffer instanceof global.AudioBuffer, "should reject with an AudioBuffer");
+            done();
+          });
+
+          assert(promise instanceof Promise, "should return a Promise");
+        });
+      });
+      describe("(invalidAudioData: ArrayBuffer, successCallback: function, errorCallback: function): Promise<AudioBuffer>", function() {
+        it("should call the errorCallback", function(done) {
+          var promise = audioContext.decodeAudioData(invalidAudioData(), function() {
+            assert(false, "NOT REACHED");
+          }, function() {
+            assert(true, "should call the errorCallback");
+            done();
+          });
+
+          assert(promise instanceof Promise, "should return a Promise");
+        });
+      });
+    });
+  });
+
+})();
