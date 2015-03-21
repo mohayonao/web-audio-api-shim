eb5bacd7ec9915c1047a0232f4bbbeb1982698e6
mohayonao <mohayonao@gmail.com>
2015-03-21 12:32:58 +0900

    add AudioContext.prototype.createStereoPanner

---
+++ test/AudioContext/createStereoPanner.js
@@ -0,0 +1,26 @@
+(function() {
+  "use strict";
+
+  require("../bootstrap");
+  require("../../src/AudioContext/createStereoPanner");
+
+  describe("AudioContext.prototype.createStereoPanner", function() {
+    var context;
+    if (!/\[native code\]/.test(global.AudioContext.prototype.createStereoPanner.toString())) {
+      context = "shim";
+    } else {
+      context = "native";
+    }
+    describe(context, function() {
+      describe("(): AudioNode", function() {
+        it("should return an AudioNode as StereoPannerNode", function() {
+          var node = audioContext.createStereoPanner();
+
+          assert(node instanceof global.AudioNode);
+          assert(node.pan instanceof global.AudioParam);
+        });
+      });
+    });
+  });
+
+})();
