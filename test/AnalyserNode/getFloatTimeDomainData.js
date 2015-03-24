174f2e786de486d172a5494c106be89195dc813b
mohayonao <mohayonao@gmail.com>
2015-03-24 21:05:46 +0900

    fix test for iOS

--- test/AnalyserNode/getFloatTimeDomainData.js
+++ test/AnalyserNode/getFloatTimeDomainData.js
@@ -9,10 +9,11 @@
 
     describe(context, function() {
       describe("(array: Float32Array): void", function() {
-        it("works", function(done) {
+        it.unless_on_iOS("works", function(done) {
           var buffer = audioContext.createBuffer(1, 4, audioContext.sampleRate);
           var bufSrc = audioContext.createBufferSource();
           var analyser = audioContext.createAnalyser();
+          var gain = audioContext.createGain();
 
           buffer.getChannelData(0).set([ -1, -1/3, +1/3, +1 ]);
 
@@ -21,9 +22,11 @@
           bufSrc.start(audioContext.currentTime);
 
           analyser.fftSize = 32;
+          gain.gain.value = 0;
 
           bufSrc.connect(analyser);
-          analyser.connect(audioContext.destination);
+          analyser.connect(gain);
+          gain.connect(audioContext.destination);
 
           setTimeout(function() {
             var array = new Float32Array(32);
@@ -39,6 +42,7 @@
               bufSrc.stop(audioContext.currentTime);
               bufSrc.disconnect();
               analyser.disconnect();
+              gain.disconnect();
               done();
             }, 0);
           }, 50);
