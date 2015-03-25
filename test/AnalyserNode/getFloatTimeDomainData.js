0153194e39bdf3d91ccae2d89d51e00a6b606d05
mohayonao <mohayonao@gmail.com>
2015-03-25 22:07:44 +0900

    update test

--- test/AnalyserNode/getFloatTimeDomainData.js
+++ test/AnalyserNode/getFloatTimeDomainData.js
@@ -6,55 +6,68 @@
 
   describe("AnalyserNode.prototype.getFloatTimeDomainData", function() {
     var context = global.getShimType(global.AnalyserNode.prototype.getFloatTimeDomainData);
+    var _ = null;
+
+    before(function() {
+      _ = {};
+      _.buffer = audioContext.createBuffer(1, 4, audioContext.sampleRate);
+      _.bufSrc = audioContext.createBufferSource();
+      _.analyser = audioContext.createAnalyser();
+      _.mute = audioContext.createGain();
+
+      _.buffer.getChannelData(0).set([ -1, -1/3, +1/3, +1 ]);
+
+      _.bufSrc.buffer = _.buffer;
+      _.bufSrc.loop = true;
+      _.bufSrc.start(audioContext.currentTime);
+
+      _.analyser.fftSize = 32;
+      _.mute.gain.value = 0;
+
+      _.bufSrc.connect(_.analyser);
+      _.analyser.connect(_.mute);
+      _.mute.connect(audioContext.destination);
+    });
+
+    function expect(fn, callback) {
+      var array = new Float32Array(32);
+
+      function ready() {
+        _.analyser.getFloatTimeDomainData(array);
+
+        if (array[0] === 0) {
+          return setTimeout(ready, 0);
+        }
+
+        setTimeout(test, 0);
+      }
+
+      function test() {
+        fn(array);
+
+        callback();
+      }
+
+      ready();
+    }
+
+    after(function() {
+      _.bufSrc.stop(audioContext.currentTime);
+      _.bufSrc.disconnect();
+      _.analyser.disconnect();
+      _.mute.disconnect();
+      _ = null;
+    });
 
     describe(context, function() {
       describe("(array: Float32Array): void", function() {
-        var buffer, bufSrc, analyser, mute;
-
-        before(function() {
-          buffer = audioContext.createBuffer(1, 4, audioContext.sampleRate);
-          bufSrc = audioContext.createBufferSource();
-          analyser = audioContext.createAnalyser();
-          mute = audioContext.createGain();
-
-          buffer.getChannelData(0).set([ -1, -1/3, +1/3, +1 ]);
-
-          bufSrc.buffer = buffer;
-          bufSrc.loop = true;
-          bufSrc.start(audioContext.currentTime);
-
-          analyser.fftSize = 32;
-          mute.gain.value = 0;
-
-          bufSrc.connect(analyser);
-          analyser.connect(mute);
-          mute.connect(audioContext.destination);
-        });
-        after(function() {
-          bufSrc.stop(audioContext.currentTime);
-          bufSrc.disconnect();
-          analyser.disconnect();
-          mute.disconnect();
-        });
         it("should copy the current time-domain (waveform) data into the passed floating-point array", function(done) {
-          var array = new Float32Array(32);
-
-          function test() {
-            analyser.getFloatTimeDomainData(array);
-
-            if (array[0] === 0) {
-              return setTimeout(test, 20);
-            }
-
+          expect(function(array) {
             assert(array[0] !== array[1]);
             assert(array[1] !== array[2]);
             assert(array[2] !== array[3]);
             assert(array[0] === array[4]);
-
-            setTimeout(done, 0);
-          }
-
-          test();
+          }, done);
         });
       });
     });
