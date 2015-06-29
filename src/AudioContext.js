3a967099b5c5cc49345c6534b855af55990776e5
mohayonao <mohayonao@gmail.com>
2015-06-29 14:39:33 +0900

    remove AudioWorkerNode support

--- src/AudioContext.js
+++ src/AudioContext.js
@@ -337,28 +337,6 @@ function replaceAudioContext() {
 }
 
 function installCreateAudioWorker() {
-  if (OriginalAudioContext.prototype.hasOwnProperty("createAudioWorker")) {
-    return;
-  }
-
-  let AudioWorkerNode = require("audio-worker-node");
-
-  //// ### AudioContext.prototype.createAudioWorker
-  //// Creates an **`AudioWorkerNode`** and its associated **`AudioWorkerGlobalScope`** for direct audio processing using JavaScript.
-  ////
-  //// #### Parameters
-  //// - `scriptURL: string`
-  ////   - This parameter represents the URL of the script to be loaded as an AudioWorker.
-  //// - `numberOfInputChannels: number = 2`
-  ////   - This parameter determines the number of channels for this node's input.
-  //// - `numberOfOutputChannels: number = 2`
-  ////   - This parameter determines the number of channels for this node's output.
-  ////
-  //// #### Return
-  //// - `AudioNode as AudioWorkerNode`
-  OriginalAudioContext.prototype.createAudioWorker = function(scriptURL, numberOfInputChannels, numberOfOutputChannels) {
-    return new AudioWorkerNode(this, scriptURL, numberOfInputChannels, numberOfOutputChannels);
-  };
 }
 
 function installCreateStereoPanner() {
